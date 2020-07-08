4e874d6e35f30f8fb7533441c20e87ddeec6f88b
Lycurgus1 <max.palmer.official@outlook.com>
2020-07-08 22:01:51 +0100

    commit

--- API_JSON_Exercise/draft_code_exercise.py
+++ API_JSON_Exercise/draft_code_exercise.py
@@ -1,15 +1,40 @@
-def print_data(self):
+import requests
+
+def print_data():
+    # Using JSON and API to get JSON dictionary
     post_codes_req = requests.get("https://api.postcodes.io/postcodes/se120nb")
     json_data = post_codes_req.json()
-    for key, value in json_data.items():
-        # value1 = json_data.items()
-        if type(value) is dict:
-            for key in json_data[value]:
+    a =
+    # For loop going through 1st dictionary
+    for key in json_data:
+        value1 = json_data.get(key)
+        # If loop for 1st dictionary, either prints values/keys or sends to other for loop if they are dictionary
+        if type(value1) is dict:
+            # For loop going through 2nd dictionary
+            for key in json_data["result"]:
                 value2 = json_data["result"].get(key)
+                # If loop for 2nd dictionary, either prints values/keys or sends to other for loop if they are dictionary
                 if type(value2) is dict:
+                    # For loop going through 3rd dictionary
                     for key in json_data["result"]["codes"]:
+                        # Printing keys and values in 3rd dictionary
                         print(key, ":", json_data["result"]["codes"].get(key))
                 else:
+                    # Print keys and values in 2nd dictionary
                     print(key, ":", value2)
-        else:
-            print(key, ":", value)
+
+# def print_data():
+#     post_codes_req = requests.get("https://api.postcodes.io/postcodes/se120nb")
+#     json_data = post_codes_req.json()
+#     for key, value in json_data.items():
+#         # value1 = json_data.items()
+#         if type(value) is dict:
+#             for key in json_data[value]:
+#                 value2 = json_data["result"].get(key)
+#                 if type(value2) is dict:
+#                     for key in json_data["result"]["codes"]:
+#                         print(key, ":", json_data["result"]["codes"].get(key))
+#                 else:
+#                     print(key, ":", value2)
+#         else:
+#             print(key, ":", value)
--- JSON/README.md
+++ JSON/README.md
@@ -1,12 +1,32 @@
 # JSON
 - Javascript object notation
-- Independent language
-
+    - Independent language
+    - Syntax for exchanging information
+    - Data comes in key value pairs, seperated by commas:
 ```
 {"Cheese": "Brie", "Meat": "Salami"}
 ```
 
-- Data separated by commas
+- Uses dump, dumps to encode. Do similar things slightly differently (JSON to python):
+```python
+# Creating JSON object- json.dumps changes python dict to json str
+# Just to illustrate theory
+car_data_json_string = json.dumps(car_data)
+# Data type has changing
+print(type(car_data_json_string))
+```
+```python
+# Create JSON file with writing permision - W = write. Column aliasing at end
+with open("new_json_file.json", "w") as jsonfile:
+    # Dump method takes two args. First one is dictionary created. Second is the file object
+    json.dump(car_data, jsonfile)
+```
+- Uses load to decode (python to JSON):
+```python
+    def conversion(self):
+        with open("exchange_rates.json", "r") as jsonfile:
+            self.dataset = json.load(jsonfile)
+```
 
 - HTTP Data types: What we get back in JSON file from request
     - Header
